0fe5696f2eeb4f6dbb3eddb940eed4cfa4a19bd2
Ondrej Sika <sika.ondrej@gmail.com>
2012-06-02 18:00:07 +0200

    add json view

---
+++ project/app/aimap/json.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.forms.models import modelformset_factory
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.cache import never_cache, cache_page
+
+from aimap.models import Person
+
+def person_all():
+    objs = []
+    for obj in Person.objects.all():
+        objs.append(obj.get_json())
+    
+    return u"[%s]"%(",".join(objs))
+
+def person_all_view(request):
+    return HttpResponse(person_all())
+
--- project/app/aimap/urls.py
+++ project/app/aimap/urls.py
@@ -11,4 +11,7 @@ urlpatterns = patterns('',
         "aimap.views.detail",
         {"template": "aimap/detail.html", },
         name="aimap.detail"),
+    url(r'json/person_all.json$',
+        "aimap.json.person_all_view",
+        name="aimap.json.person_all"),
 )
