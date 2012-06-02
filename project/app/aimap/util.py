d8586551c07d708ba015f12a7c28d6060b423358
frrp <frantisek.polach@gmail.com>
2012-06-02 08:46:26 -0400

    add util

---
+++ project/app/aimap/util.py
@@ -0,0 +1,6 @@
+import unicodedata
+
+def unicode2ascii(string):
+    """Converts unicode string to ascii as close as possible"""
+    return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore')
+
