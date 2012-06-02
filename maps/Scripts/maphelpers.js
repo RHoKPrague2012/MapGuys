901b789759998eddc1eaf40f303f61dce095f3f0
Capaj <capajj@gmail.com>
2012-06-02 13:41:13 +0200

    first map can be seen, if you have an API key

---
+++ maps/Scripts/maphelpers.js
@@ -0,0 +1,8 @@
+﻿function ShowMap() {
+    var map = new L.Map('map');
+    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/YOUR-API-KEY/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    });
+    map.addLayer(cloudmade);
+}
