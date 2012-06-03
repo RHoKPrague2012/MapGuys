9ba483328be905e66ce2ba66ec9ea0b052a92244
Ondrej Sika <sika.ondrej@gmail.com>
2012-06-03 09:48:26 +0200

    rm from form unnecessary field

--- project/app/aimap/admin.py
+++ project/app/aimap/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from models import *
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    exclude = ("ascii_first_name", "ascii_last_name", "ascii_issue_name", )
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.owner = request.user
+        obj.save()
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Country)
 
 		 
