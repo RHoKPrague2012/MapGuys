7c50ca5e1660d25069ec241b0e1d57629ab60641
Ondrej Sika <sika.ondrej@gmail.com>
2012-06-03 09:12:53 +0200

    add detail view

--- project/app/aimap/views.py
+++ project/app/aimap/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.cache import never_cache, cache_page
 
 
+from models import Person
+
 def home(request, template):
     dictionary = {}
     return render_to_response(template, dictionary,
@@ -15,6 +17,7 @@ def home(request, template):
 
 
 def detail(request, pk, template):
-    dictionary = {}
+    person = Person.objects.get(pk=pk)
+    dictionary = {"person": person}
     return render_to_response(template, dictionary,
                               context_instance=RequestContext(request))
