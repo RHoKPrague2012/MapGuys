da9cd6c3a0f77bef3da471d9d46435762b2a80f3
Ondrej Sika <sika.ondrej@gmail.com>
2012-06-03 15:49:36 +0200

    add load time

--- project/app/aimap/helpers.py
+++ project/app/aimap/helpers.py
@@ -1,24 +1,28 @@
 from app.aimap.models import *
 import datetime
+import time
 def load():
     import app
     i = 0
     f = file("/home/sika/data2.csv", "r")
     for line in f.read().split("\n"):
         i += 1
-        #if i == 10: break
+        if i == 10: break
         try:
             d = line.split(";")
+            a = d[6].split(" ")
+            dd = "%s-%s-%s"%(a[2], time.strptime(a[1], "%b").tm_mon, a[0])
             print d[1]
             obj = Person(issue_name=d[3],
                          description=d[4],
+                         country=d[0],
                          ai_library=d[5],
                          lat=float(d[1]),
                          lon=float(d[2]),
-                         issue_date=datetime.date.today()
+                         issue_date=dd
                          )
             obj.save()
-        except:
+        except ValueError:
             pass
                         
 def lw(request):
